f13c4c340bf4a57fb036b00daa345dc3fd0b0f68
ViSiOnX10 <136606140+ViSiOnX10@users.noreply.github.com>
2023-06-14 19:05:25 +0530

    Create MajorityElement.java
    Java code for Majority Element for DSA Essentials

---
+++ MajorityElement.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MajorityElement {
+    public static int findMajorityElement(int[] nums) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+        int majorityThreshold = nums.length / 2;
+
+        for (int num : nums) {
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+
+            if (countMap.get(num) > majorityThreshold) {
+                return num;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 4, 5, 5, 5, 5, 5};
+        int majorityElement = findMajorityElement(nums);
+        System.out.println("Majority element: " + majorityElement);
+    }
+}
